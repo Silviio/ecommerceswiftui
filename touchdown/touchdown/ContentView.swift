283a1cb96800f34289aae4a42fbc476f9be7ad1f
Silvio Nogueira Martins <silvionogueiramartins@Silvios-MacBook-Air.local>
2021-10-25 22:11:22 -0300

    start do projeto

---
+++ touchdown/touchdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  touchdown
+//
+//  Created by Silvio Nogueira Martins on 25/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ touchdown/touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by Silvio Nogueira Martins on 25/10/21.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
