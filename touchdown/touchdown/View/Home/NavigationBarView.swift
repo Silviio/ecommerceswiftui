2d6fa9708f4d818c2dccf9aabbd7920b0bca105e
Silvio Nogueira Martins <martinsnsil@gmail.com>
2021-11-01 12:37:50 -0300

    navigation com o logo

---
+++ touchdown/touchdown/View/Home/NavigationBarView.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationBarView.swift
+//  touchdown
+//
+//  Created by Silvio Nogueira Martins on 01/11/21.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+    }
+}
